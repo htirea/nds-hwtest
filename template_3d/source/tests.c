8ddb48442728492834094c79ce537bfd5d2499b9
Azusa <azusachan1111@gmail.com>
2024-04-06 17:47:26 +1100

    change function order

--- template_3d/source/tests.c
+++ template_3d/source/tests.c
@@ -4,6 +4,19 @@
 #include <stdio.h>
 
 void
+gfx_vertex16(int x, int y, int z)
+{
+	GFX_VERTEX16 = (y << 16 & 0xFFFF0000) | (x & 0xFFFF);
+	GFX_VERTEX16 = (z & 0xFFFF);
+}
+
+void
+gfx_color(int r, int g, int b)
+{
+	GFX_COLOR = b << 10 | g << 5 | r;
+}
+
+void
 setup_test()
 {
 	glClearColor(0, 0, 0, 0);
@@ -18,19 +31,6 @@ setup_test()
 	MATRIX_CONTROL = 3;
 	MATRIX_IDENTITY = 0;
 	glViewport(VIEWPORT_X0, VIEWPORT_Y0, VIEWPORT_X1, VIEWPORT_Y1);
-}
-
-void
-gfx_vertex16(int x, int y, int z)
-{
-	GFX_VERTEX16 = (y << 16 & 0xFFFF0000) | (x & 0xFFFF);
-	GFX_VERTEX16 = (z & 0xFFFF);
-}
-
-void
-gfx_color(int r, int g, int b)
-{
-	GFX_COLOR = b << 10 | g << 5 | r;
 }
 
 /*
